f4e49c6271176fdc1e75f5dbb59e3a38bcf70fb3
Tnose <dls.y.nose@gmail.com>
2022-04-21 07:32:05 +0900

    [AC] abs abc083_b

---
+++ atcoder.jp/abs/abc083_b/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace abc083b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Trim().Split(' ')
+                .Select(int.Parse).ToArray();
+            int n = input[0];
+            int a = input[1];
+            int b = input[2];
+            int sum = 0;
+
+            for(int i=1;i<n+1;i++){
+                int s = i/10000%10 + i/1000%10 + i/100%10 + i/10%10 + i%10;
+                if(a <= s && s <= b){
+                    sum = sum + i;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
