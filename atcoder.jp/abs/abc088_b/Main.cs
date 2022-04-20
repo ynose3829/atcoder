131f68d5e913168b94191476b6ab3b5e58249c29
Tnose <dls.y.nose@gmail.com>
2022-04-21 07:58:37 +0900

    [AC] abs abc088_b

---
+++ atcoder.jp/abs/abc088_b/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace abc088b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            List<int> a  = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList<int>();
+            int alice = 0;
+            int bob = 0;
+            bool flg = true;
+
+            while(flg){
+                alice += a.Max();
+                a.Remove(a.Max());
+
+                if(a.Count == 0) break;
+
+                bob += a.Max();
+                a.Remove(a.Max());
+
+                if(a.Count == 0) break;
+            }
+
+
+            Console.WriteLine(alice-bob);
+        }
+    }
+}
