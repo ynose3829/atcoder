9ae449654cce2255a1494837f884fd56b7d760e6
Tnose <dls.y.nose@gmail.com>
2022-04-22 19:39:38 +0900

    [AC] abs abc085_c

---
+++ atcoder.jp/abs/abc085_c/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        int n = int.Parse(input[0]);
+        int y = int.Parse(input[1]);
+ 
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= n - i; j++)
+            {
+                int k = n - i - j;
+                if (y == 10000 * i + 5000 * j + 1000 * k)
+                {
+                    Console.WriteLine($"{i} {j} {k}");
+                    return;
+                }
+            }
+        }
+ 
+        Console.WriteLine("-1 -1 -1");
+    }
+}
