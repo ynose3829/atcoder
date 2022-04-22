865b95175316fd576477d918a81c7eb2d8bbfa7b
Tnose <dls.y.nose@gmail.com>
2022-04-22 18:43:21 +0900

    [AC] abs abc085_b

---
+++ atcoder.jp/abs/abc085_b/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace abc085b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 1;
+            int n = int.Parse(Console.ReadLine().Trim());
+            int[] d = new int[n];
+            for(int i=0;i<n;i++){
+                d[i] = int.Parse(Console.ReadLine().Trim());
+            }
+            Array.Sort(d);
+            Array.Reverse(d);
+
+            for(int i=0;i<n-1;i++){
+                if(d[i] > d[i+1]){
+                    cnt++;
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
