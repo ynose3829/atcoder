15d2952bb1f12b585581fa8da602f51ca33d1ea8
Tnose <dls.y.nose@gmail.com>
2022-04-23 21:34:48 +0900

    [AC] abc249 abc249_b

---
+++ atcoder.jp/abc249/abc249_b/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().Trim();
+            bool flg = true;
+            bool isUpper = false;
+            bool isLower = false;
+
+
+            for(int i=0; i<s.Length-1;i++){
+                for(int j=0; j<s.Length-1;j++){
+                    if(s[i] == s[j] && i != j){
+                        flg = false;
+                    }
+                }
+            }
+
+            foreach(char c in s){
+                isUpper = isUpper || char.IsUpper(c);
+                isLower = isLower || char.IsLower(c);
+            }
+
+
+
+            if(flg && isLower && isUpper){Console.WriteLine("Yes");}
+            else{Console.WriteLine("No");}
+        }
+    }
+}
