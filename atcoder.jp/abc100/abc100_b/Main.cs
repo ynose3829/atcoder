759f130a5e1760d440a397e969d37c856b1615da
Tnose <dls.y.nose@gmail.com>
2022-05-17 23:22:35 +0900

    [AC] abc100 abc100_b

---
+++ atcoder.jp/abc100/abc100_b/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] tmp = Console.ReadLine().Trim().Split(' ').Select(double.Parse).ToArray();
+            double d = tmp[0];
+            double n = tmp[1];
+            
+            
+
+            if(n != 100){
+                double x = Math.Pow(100,d)*n;
+                Console.WriteLine(x);
+            }else if(n == 100){
+                double x = Math.Pow(100,d)*101;
+                Console.WriteLine(x);
+            }
+            
+        }
+    }
+}
