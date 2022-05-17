8b8ccf56bd14c0ee3fd1b1441d4e9096e88a0727
Tnose <dls.y.nose@gmail.com>
2022-05-17 22:50:03 +0900

    [AC] abc100 abc100_a

---
+++ atcoder.jp/abc100/abc100_a/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tmp = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            int a = tmp[0];
+            int b = tmp[1];
+
+            if((a <= 8) && (b <= 8)){
+                Console.WriteLine("Yay!");
+            }else{
+                Console.WriteLine(":(");
+            }
+           
+        }
+    }
+}
