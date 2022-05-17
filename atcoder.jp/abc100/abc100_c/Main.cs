26921aa57a6e2777b445f8aa13deae67fc3b091f
Tnose <dls.y.nose@gmail.com>
2022-05-17 23:41:15 +0900

    [AC] abc100 abc100_c

---
+++ atcoder.jp/abc100/abc100_c/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            int[] a = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            int cnt = 0;
+
+            for(int i=0;i<n;i++){
+                while(a[i] % 2 == 0){
+                    a[i] /= 2;
+                    cnt++;
+                }
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
