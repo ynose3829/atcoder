e1493b2050beede152c0e6d154667af9722a6506
Tnose <dls.y.nose@gmail.com>
2022-05-21 21:24:09 +0900

    [AC] abc252 abc252_b

---
+++ atcoder.jp/abc252/abc252_b/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tmp = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            int n = tmp[0];
+            int k = tmp[1];
+
+            int[] A = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            int[] B = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+
+            bool flg = false;
+            
+            int max = 0;
+            foreach(int a in A){
+                if(max < a) max = a;
+            }           
+
+            foreach(int b in B){
+                if(A[b-1] == max) flg =true;
+            }
+
+
+            if(flg){
+                Console.WriteLine("Yes");
+            }else if(!flg){
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
