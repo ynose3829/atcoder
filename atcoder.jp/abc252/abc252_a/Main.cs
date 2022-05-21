e1b23701ba396a8c004910d74c7bd2802dd6abdf
Tnose <dls.y.nose@gmail.com>
2022-05-21 21:04:33 +0900

    [AC] abc252 abc252_a

---
+++ atcoder.jp/abc252/abc252_a/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            char ans = ((char)n);
+
+            Console.WriteLine(ans);
+        }
+    }
+}
