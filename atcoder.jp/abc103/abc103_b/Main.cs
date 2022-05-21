e89656a53cd53341dd1da1b6a8cd89a0065e405b
Tnose <dls.y.nose@gmail.com>
2022-05-21 12:13:51 +0900

    [AC] abc103 abc103_b

---
+++ atcoder.jp/abc103/abc103_b/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().Trim();
+            string t = Console.ReadLine().Trim();
+            int n = s.Length;
+            bool flg = false;
+
+            for(int i=0;i<n;i++){
+                char tmp = s[n-1];
+                string tmp2 =   s.Substring(0,n-1);
+
+                s = tmp.ToString();
+                s += tmp2;
+                if(s.Contains(t))flg = true;
+            }
+
+            if(flg) Console.WriteLine("Yes");
+            if(!flg)Console.WriteLine("No");
+        }
+    }
+}
