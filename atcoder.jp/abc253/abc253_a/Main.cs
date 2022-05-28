bdd50a35e261c0df554e1862de890c54e5ddbec8
Tnose <dls.y.nose@gmail.com>
2022-05-28 21:07:57 +0900

    [AC] abc253 abc253_a

---
+++ atcoder.jp/abc253/abc253_a/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tmp = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            int a = tmp[0];
+            int b = tmp[1];
+            int c = tmp[2];
+
+            Array.Sort(tmp);
+            
+            if(b == tmp[1])Console.WriteLine("Yes");
+            if(b != tmp[1])Console.WriteLine("No");
+        }
+    }
+}
