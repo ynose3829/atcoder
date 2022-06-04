7b1d01c0d1f6236a7110ed093c2f743ee7298eb7
Tnose <dls.y.nose@gmail.com>
2022-06-04 21:06:34 +0900

    [AC] abc254 abc254_a

---
+++ atcoder.jp/abc254/abc254_a/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            string a = (n % 10).ToString();
+            string b = (n / 10 % 10).ToString();
+
+            Console.WriteLine($"{b}{a}");
+        }
+    }
+}
