8771b3649dec423eb64dbaddc68c6cc9407e7f36
Tnose <dls.y.nose@gmail.com>
2022-06-10 00:14:10 +0900

    [AC] abc243 abc243_a

---
+++ atcoder.jp/abc243/abc243_a/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmp = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            int v = tmp[0];
+            int a = tmp[1];
+            int b = tmp[2];
+            int c = tmp[3];
+
+            int sum = a+b+c;
+            int mod = v % sum;
+
+            if(mod < a){
+                Console.WriteLine("F");
+            }else if(mod < a+b){
+                Console.WriteLine("M");
+            }else if(mod < sum){
+                Console.WriteLine("T");
+            }
+
+
+        }
+    }
+}
