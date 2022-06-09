37a552fe36a7824a49f026c7e51cfe8ae603cfa5
Tnose <dls.y.nose@gmail.com>
2022-06-09 23:40:13 +0900

    [AC] abc244 abc244_d

---
+++ atcoder.jp/abc244/abc244_d/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine().Trim();
+            string T = Console.ReadLine().Trim();
+            bool flg1 = sign(S);
+            bool flg2 = sign(T);
+
+            if(flg1 ^ flg2){
+                Console.WriteLine("No");
+            }else{
+                Console.WriteLine("Yes");
+            }
+            
+        }
+
+        static bool sign(string S){
+            return S == "R G B" || S == "G B R" || S == "B R G";
+        }
+    }
+}
