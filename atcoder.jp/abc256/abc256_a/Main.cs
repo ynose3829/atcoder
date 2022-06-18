02d2f665a65e186fa1437d95426e3688f69e7370
Tnose <dls.y.nose@gmail.com>
2022-06-18 21:03:08 +0900

    [AC] abc256 abc256_a

---
+++ atcoder.jp/abc256/abc256_a/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+
+            double ans = Math.Pow(2,n);
+
+            Console.WriteLine(ans);
+        }
+    }
+}
