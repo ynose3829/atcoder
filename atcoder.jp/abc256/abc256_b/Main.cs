abd5d258ddd272a8a283a21cd223583a2eec9ca7
Tnose <dls.y.nose@gmail.com>
2022-06-18 21:26:33 +0900

    [AC] abc256 abc256_b

---
+++ atcoder.jp/abc256/abc256_b/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var A = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            int P = 0;
+            bool[] dp = new bool[4];
+
+
+            for(int i=0;i<n;i++){
+                dp[0] = true;
+                for(int j=3;j>=0;j--){
+                    if(dp[j]){
+                        if(j + A[i] < 4){
+                            dp[j+A[i]] = true;
+                        }else{
+                            P++;
+                        }
+                        dp[j] = false;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine($"{P}");
+
+        }
+    }
+}
